ce4d7aa6aeb40cda827d458962d41ec0717e7a90
KB <koftyan@mail.ru>
2016-10-31 17:33:59 +0300

    test passportJs local authentication with l:admin p:admin

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 var config = require("nconf");
 var express = require("express");
-var expressSession = require('express-session');
-var passport = require("passport"), LocalStrategy = require('passport-local').Strategy;
+//var expressSession = require('express-session');
+var passport = require("passport"); 
+var LocalStrategy = require('passport-local');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 var app = express();
+var bodyParser = require('body-parser');
 
 config.argv()
 	.env()
 	.file({file: 'config.json'});
 
-app.use(expressSession({secret: config.get("app").secret}));
+//app.use(expressSession({secret: config.get("app").secret}));
+app.use(cookieParser()); // req.cookies
+app.use(session({keys: [config.get("app").secret]})); // req.session
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-	done(null, user._id);
+// Метод сохранения данных пользователя в сессии
+passport.serializeUser(function (user, done) {
+  	done(null, user.username);
 });
 
-passport.deserializeUser(function(id, done) {
-	// User.findById(id, function(err, user) {
-	// 	done(err, user);
-	// });
-	var user = {username: "admin", password: "admin"};
-	done("error in deserialize", user);
+// Метод извлечения данных пользователя из сессии
+passport.deserializeUser(function (username, done) {
+  	done(null, {username: username});
 });
 
-passport.use('login', new LocalStrategy({
-	function(username, password, done) {
-		if(username != "admin") {
+// Настройка стратегии авторизации
+passport.use(new LocalStrategy(function (username, pass, done) {
+	// Проверяем авторизационные данные
+	if (username === 'admin' && pass === 'admin')
+		return done(null, {username: username});
 
-		}
-	}
+	done(null, false);
 }));
 
 app.set('port', config.get("app").port);
@@ -38,14 +43,38 @@ app.get('/', function(req, res) {
 	res.send('hello world');
 });
 
-app.get('/secret', function(req, res) {
-	passport.authenticate('local', {successRedirect: '/secret', failtureRedirect: '/login'});
-	res.send("secret part");
+app.get('/secret', mustBeAuthentificated, function(req, res) {
+	//passport.authenticate('local', {successRedirect: '/secret', failtureRedirect: '/login'});
+	res
+		.status(200)
+		.send("secret part");
 });
 
-app.get('login', function(req, res) {
-	res.send("You need to login");
+app.get('/login', function(req, res) {
+	res
+		.status(200)
+		.send(
+			'<form action="/login" method="post">' +
+		        'Login: ' +
+		        '<input type="text" name="username" />' +
+		        '<input type="password" name="password" />' +
+		        '<input type="submit" />' +
+      		'</form>');
 });
+
+// Обработчик запроса на авторизацию
+app.post('/login', bodyParser.urlencoded({ extended: false }), passport.authenticate('local', {
+  successRedirect: '/secret',
+  failureRedirect: '/login'
+}));
+
+// Метод для проверки пользователя
+function mustBeAuthentificated (req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+
+	res.redirect('/login'); // переход на страницу логина
+}
 
 app.listen(app.get('port'), function() {
 	console.log('app was started on ' + app.get('port') + ' port!');
